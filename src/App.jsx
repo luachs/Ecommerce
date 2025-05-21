648fcd0c5eb9ac06aa2bff8716e3b3ccfca9f0d0
Phat <nguyenthanhphat13032005@gmail.com>
2025-05-21 22:27:53 +0700

    update hero

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Product from "./components/Product/Product";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Product />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div>
+      <div>
+        {/* Header section */}
+        <div className="text-center mb-10">
+          <p>Top Selling Products for you</p>
+          <h1>Product</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            ipsum totam iste sapiente incidunt velit laudantium ex itaque labore
+            accusamus eos, excepturi atque molestias at, cum cumque eveniet
+            pariatur aut.
+          </p>
+        </div>
+        {/* body section */}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
